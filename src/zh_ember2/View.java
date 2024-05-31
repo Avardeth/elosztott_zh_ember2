cee03146a0d6fd248d1f86d3836003152ba1fb52
Avardeth <kosagyula128@gmail.com>
2024-05-31 19:20:50 +0200

    minor fix

--- src/zh_ember2/View.java
+++ src/zh_ember2/View.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 package zh_ember2;
 
-/**
- *
- * @author kali
- */
 public class View extends javax.swing.JFrame {
 
     /**
